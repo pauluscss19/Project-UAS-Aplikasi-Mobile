89ce1372608a26120003b058031ceaccd18fe83c
PAULUS CALEP SANDRIA SAPUTRA <147365443+pauluscss19@users.noreply.github.com>
2025-12-07 04:03:02 +0700

    Update API base URL for storage API

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Ganti dengan IP komputer Anda
-const API_BASE_URL = 'http://ISI IP/storage_api';
+const API_BASE_URL = 'http://ISI IP/uas-mobile/storage_api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
